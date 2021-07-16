36e828bb5e295f772b2c7de54816a3b1a6cb1aa4
jradler-wassoc <jradler@wassoc.com>
2021-07-15 17:25:14 -0700

    Stubbed out some bootstrapping scripts for provisioning and configuring the dev environment.

---
+++ 0_powershell_tools.ps1
@@ -0,0 +1,3 @@
+# 0_powershell_tools.ps1
+Write-Output "Setting up Powershell for Marine Induced Polarization AVR Development environment..."
+
---
+++ 0_powershell_tools.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 0_powershell_tools.psl
+Write-Output "test_output" | Get-Member
+# Download and install Chocolatey automatically?
+
